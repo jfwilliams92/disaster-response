efd5d398d5d6c1dd584e5489f986afd1a7bc5b32
James Williams <jwilliams1@resurgent.com>
2020-02-13 15:35:36 -0500

    change app structure

--- disaster_response_app/run.py
+++ disaster_response_app/run.py
@@ -11,26 +11,18 @@ from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
+from os import path
+import sys
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from message_tokenizer import MessageTokenizer
 
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
-
-# load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///data/DisasterMessages.db')
+df = pd.read_sql_table('CleanMessages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("models/disaster_logit.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -73,7 +65,6 @@ def index():
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
-
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
@@ -93,8 +84,8 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
-
+    #app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
