01762e43f63ed969e068bd3ceadb8ae25b168975
James Williams <jwilliams1@resurgent.com>
2020-02-13 23:47:31 -0500

    allow hard and soft prediction

--- disaster_response_app/run.py
+++ disaster_response_app/run.py
@@ -2,9 +2,6 @@ import json
 import plotly
 import pandas as pd
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
@@ -23,7 +20,6 @@ df = pd.read_sql_table('CleanMessages', engine)
 
 # load model
 model = joblib.load("models/disaster_logit.pkl")
-
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
@@ -66,30 +62,38 @@ def index():
     # print('here at the index page!')
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
-
-
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
     query = request.args.get('query', '') 
-
+    # classification type comes from the radio button selection
+    classification_type = request.args.get('classification_type', 'hard_class')
+    
     # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
+    # 0 1 prediction for 'hard_class'
+    if classification_type == 'hard_class':
+        classification_labels = model.predict([query])[0]
+    # predict label probability for 'soft_class'
+    elif classification_type == 'soft_class':
+        classification_labels = model.predict_proba([query])[0]
+    
+    # filter to labels with any positive instances
+    # this is what happens during classifier training - otherwise there will be a prediction-label mismatch
     df_labels = (df.iloc[:, 4:] == 1).any(axis=0) 
     classification_keys = list(df_labels[df_labels.values].index)
     classification_results = dict(zip(classification_keys, classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # pass the data back to the go.html page 
     return render_template(
         'go.html',
         query=query,
-        classification_result=classification_results
+        classification_result=classification_results,
+        classification_type=classification_type
     )
 
 
 def main():
-    #app.run(host='0.0.0.0', port=3001, debug=True)
     app.run(debug=True)
 
 if __name__ == '__main__':
